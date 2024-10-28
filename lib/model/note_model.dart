656b634a8dbe4ffafc6b565bf27004e0cd977485
Asnais-gh <e.greo75@gmail.com>
2024-10-28 22:47:28 +0200

    create note model

---
+++ lib/model/note_model.dart
@@ -0,0 +1,8 @@
+class NoteModel {
+  final String tilte;
+  final String subtitle;
+  final String date;
+  final int color;
+
+  NoteModel({required this.tilte, required this.subtitle, required this.date, required this.color});
+}
